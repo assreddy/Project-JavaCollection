e8cdc4fa7f6f42026fb9239270aac6cbea6d40c0
Sree <ambati.sreenu@gmail.com>
2018-01-08 20:50:00 -0800

    Update Customer.java

--- src/main/java/com/springframework/model/Customer.java
+++ src/main/java/com/springframework/model/Customer.java
@@ -5,24 +5,40 @@ public class Customer {
 	private int customerID;
 	private String lastName;
 	private String firstName;
-	private int age;
+	private String age;
+	private int pinCode;
 
 	public Customer() {}
 
-	public Customer(int customerID, String lastName, String firstName, int age) {
-		super();
+	public Customer(int customerID, String lastName, String firstName, String age, int pinCode) {
 		this.customerID = customerID;
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.age = age;
+		this.pinCode = pinCode;
 	}
 
+
+	public Customer(int customerID, String age) {
+		this.customerID = customerID;
+		this.age = age;
+	}
+	
+	// Getter and Setter Methods
 	public int getCustomerID() {
 		return customerID;
 	}
 
 	public void setCustomerID(int customerID) {
 		this.customerID = customerID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
 	public String getLastName() {
@@ -33,25 +49,29 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public String getfirstName() {
-		return firstName;
-	}
-
-	public void setfirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public int getAge() {
+	public String getAge() {
 		return age;
 	}
 
-	public void setAge(int age) {
+	public void setAge(String age) {
 		this.age = age;
 	}
 
+	public int getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(int pinCode) {
+		this.pinCode = pinCode;
+	}
+
+	// Print Customer Info in String Format.
 	public String printCustomer() {
-		String info = String.format("custId = %d, firstname = %s, lastname = %s, age = %d", customerID, firstName, lastName, age);
+		String info = String.format("custId = %d, firstname = %s, lastname = %s, age = %d, pin= %d"
+				, customerID, firstName, lastName, age,pinCode);
 		return info;
 	}
+
+
 
 }
