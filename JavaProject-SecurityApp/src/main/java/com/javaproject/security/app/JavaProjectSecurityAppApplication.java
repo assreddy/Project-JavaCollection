dd0b628012a19f4171922106af14d29ee59b68f3
Sree <ambati.sreenu@gmail.com>
2018-01-10 17:04:20 -0800

    Add files via upload

---
+++ JavaProject-SecurityApp/src/main/java/com/javaproject/security/app/JavaProjectSecurityAppApplication.java
@@ -0,0 +1,12 @@
+package com.javaproject.security.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectSecurityAppApplication.class, args);
+	}
+}
